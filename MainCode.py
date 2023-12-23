7352356fbda623385fdc7ff1f0bfbc0eb015ae32
David <83611@siswa.unimas.my>
2023-12-23 11:34:15 +0800

    Update MainCode.py
    added class for notification and messaging

--- MainCode.py
+++ MainCode.py
@@ -11,8 +11,40 @@ class User:
     def login(self):
         print("User "+self.userName+" is login")
 class Jobseeker:
+    pass
 
+class NotificationSettings:
+    def __init__(self):
+        self.email_notifications = False
+        self.push_notifications = False
 
+    def set_email_notifications(self, enabled):
+        self.email_notifications = enabled
+
+    def set_push_notifications(self, enabled):
+        self.push_notifications = enabled
+
+class Message:
+    def __init__(self, content, timestamp):
+        self.content = content
+        self.timestamp = timestamp
+        self.is_read = False
+
+    def send_message(self, recipient):
+        # Send message to recipient                             LATER
+        pass
+
+    def received_message(self, sender):
+        # Mark message as received                   LATER      
+        pass
+
+    def view_message(self, message):
+        # Display message content                      LATER            
+        pass
+
+    def notif_message(self, message):
+        # Display message notification                     LATER
+        pass
 
 user_1 = User("82580","Harold","haroldhenry9903@gmail.com","harolH","pic","boy")
 
