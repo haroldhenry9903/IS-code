47fb9f612340d47b82ef4a809d8676258cf8ab40
haroldhenry9903 <136260372+haroldhenry9903@users.noreply.github.com>
2023-12-27 09:52:43 +0800

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -1,26 +1,51 @@
 
+import datetime
+
+#class user
 class User:
-    def __init__(self,userID,userName,email,password,profilepicture,biodata):
+
+    def __init__(self,userID,userName,email,password,profilepicture,biodata,contactDetails):
         self.userID = userID
         self.userName = userName
         self.email = email
         self.password = password
         self.profilepicture = profilepicture
         self.biodata = biodata
+        self.contactDetails = contactDetails
+
 
     def login(self):
-        print("User "+self.userName+" is login")
-class Jobseeker:
+        print("User "+self.userName+" is login\n")
+#Customer Class
+class Customer(User):
+
+    def __init__(self, userID, userName, email, password, profilepicture, biodata , contactDetails, cart , orderHistory):
+        User.__init__(self, userID, userName, email, password, profilepicture, biodata , contactDetails)
+        self.cart = cart
+        self.orderHistory = orderHistory
+
+    def viewCart(self):
+        print("Customer " + self.userName + " is Viewing cart\n")
+#Jobseeker Class
+class JobSeeker(User):
+    def __init__(self, userID, userName, email, password, profilepicture, biodata , contactDetails):
+        User.__init__(self, userID, userName, email, password, profilepicture, biodata , contactDetails)
+    def SendResume(self):
+        print("User is sending resume")
+
+#JobProvider Class
+class JobProvider(User):
+    def __init__(self, userID, userName, email, password, profilepicture, biodata , contactDetails , company , joblistings ):
+        User.__init__(self, userID, userName, email, password, profilepicture, biodata , contactDetails)
+        self.company = company
+        self.joblistings = joblistings
+
+#Artisan Class
+class Artisan(User):
+    class Artisan(User):
+        def __init__(self, userID, userName, email, password, profilepicture, biodata, contactDetails , productlistings):
+            User.__init__(self, userID, userName, email, password, profilepicture, biodata, contactDetails)
+            self.productlistings = productlistings
 
 
 
-user_1 = User("82580","Harold","haroldhenry9903@gmail.com","harolH","pic","boy")
-
-print(user_1.userID)
-print(user_1.userName)
-print(user_1.email)
-print(user_1.password)
-print(user_1.profilepicture)
-print(user_1.biodata)
-
-user_1.login()
